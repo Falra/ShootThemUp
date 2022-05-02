f50d5b90cdd1fcd5779d2d573a3ac9b5ed08cb93
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-05-02 09:37:09 +0300

    #3.24 Add blank character and controller classes

---
+++ Source/ShootThemUp/Private/Player/VDBaseCharacter.cpp
@@ -0,0 +1,29 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+
+#include "Player/VDBaseCharacter.h"
+
+// Sets default values
+AVDBaseCharacter::AVDBaseCharacter()
+{
+    // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AVDBaseCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+// Called every frame
+void AVDBaseCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AVDBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
---
+++ Source/ShootThemUp/Public/Player/VDBaseCharacter.h
@@ -0,0 +1,28 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "VDBaseCharacter.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API AVDBaseCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    // Sets default values for this character's properties
+    AVDBaseCharacter();
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+public:
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
