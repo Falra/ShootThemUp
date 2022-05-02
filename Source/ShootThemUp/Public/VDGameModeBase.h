f42bf908a35897a543e1e8bbc96e82f78f1f2041
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-05-02 08:33:05 +0300

    #3.22 Initial setup, adding content

---
+++ Source/ShootThemUp/Public/VDGameModeBase.h
@@ -0,0 +1,17 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VDGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API AVDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
