d8514321a116a0ae140aa62fa46e60ffcce627bb
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-05-09 16:05:21 +0300

    #7.78 Add pickup rotation

--- Source/ShootThemUp/Private/Pickups/VDBasePickup.cpp
+++ Source/ShootThemUp/Private/Pickups/VDBasePickup.cpp
@@ -24,6 +24,8 @@ void AVDBasePickup::BeginPlay()
     Super::BeginPlay();
 
     check(CollisionComponent);
+
+    GenerateRotationYaw();
 }
 
 void AVDBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
@@ -40,6 +42,8 @@ void AVDBasePickup::NotifyActorBeginOverlap(AActor* OtherActor)
 void AVDBasePickup::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
+
+    AddActorLocalRotation(FRotator(0.0f, RotationYaw, 0.0f));
 }
 
 bool AVDBasePickup::GivePickupTo(APawn* PlayerPawn)
@@ -58,6 +62,13 @@ void AVDBasePickup::PickupWasTaken()
 
 void AVDBasePickup::Respawn()
 {
+    GenerateRotationYaw();
     GetRootComponent()->SetVisibility(true, true);
     CollisionComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+}
+
+void AVDBasePickup::GenerateRotationYaw()
+{
+    const auto Direction = FMath::RandBool() ? 1.0f : -1.0f;
+    RotationYaw = FMath::RandRange(1.0f, 2.0f) * Direction;
 }
