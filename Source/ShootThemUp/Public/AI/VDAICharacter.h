72b426552facf5d6963aba88252ce46c2872014e
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-05-11 09:14:54 +0300

    #9.93 Blank classes/bp for AI

---
+++ Source/ShootThemUp/Public/AI/VDAICharacter.h
@@ -0,0 +1,13 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/VDBaseCharacter.h"
+#include "VDAICharacter.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API AVDAICharacter : public AVDBaseCharacter
+{
+    GENERATED_BODY()
+};
---
+++ Source/ShootThemUp/Public/AI/VDAIController.h
@@ -0,0 +1,13 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "VDAIController.generated.h"
+
+UCLASS()
+class SHOOTTHEMUP_API AVDAIController : public AAIController
+{
+    GENERATED_BODY()
+};
