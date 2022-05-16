bf5fb3428de225a74a2e9ade56aad93bba6a91c6
Valerii Diachenko <leriy.dyachenko@gmail.com>
2022-05-16 18:04:45 +0300

    #11.135 Assets and WBP for level item widget

---
+++ Source/ShootThemUp/Public/Menu/UI/VDLevelItemWidget.h
@@ -0,0 +1,17 @@
+// Shoot Them Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "VDLevelItemWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API UVDLevelItemWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
